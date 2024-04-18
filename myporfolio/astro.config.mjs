0bc130fabda19ce74f9112e6387d26b6bfc0d717
VikusS3 <felix21soc@gmail.com>
2024-04-18 10:32:55 -0500

    Update package.json, Header.astro, Hero.astro, astro.config.mjs, Projects.astro, data.json, BentoItem.astro, pnpm-lock.yaml, and Skills.astro files

--- myporfolio/astro.config.mjs
+++ myporfolio/astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import animations from '@midudev/tailwind-animations';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  plugins: [animations],
 });
