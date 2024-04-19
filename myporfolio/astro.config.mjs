e45c7ebd98c1686be782ec65abe2047f01084930
VikusS3 <felix21soc@gmail.com>
2024-04-19 10:06:43 -0500

    add animate for hero

--- myporfolio/astro.config.mjs
+++ myporfolio/astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
-import animations from '@midudev/tailwind-animations';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  plugins: [animations],
+  plugins: [],
 });
