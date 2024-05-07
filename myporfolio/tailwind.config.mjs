cb84c4636102c8b317b50247522837f385a61d74
VikusS3 <felix21soc@gmail.com>
2024-05-07 10:46:28 -0500

    add new style for projects

--- myporfolio/tailwind.config.mjs
+++ myporfolio/tailwind.config.mjs
@@ -1,9 +1,30 @@
-import animations from '@midudev/tailwind-animations';
+import animations from "@midudev/tailwind-animations";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [animations],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#FFFFFF",
+          200: "#e0e0e0",
+          300: "#9b9b9b",
+        },
+        accent: {
+          100: "#FFD700",
+          200: "#917800",
+        },
+        text: {
+          100: "#FFFFFF",
+          200: "#e0e0e0",
+        },
+        bg: {
+          100: "#000000",
+          200: "#161616",
+          300: "#2c2c2c",
+        },
+      },
+    },
+  },
+  plugins: [animations],
+};
